4725e1cd8fcf9f0dc13c48a12fad205f2936b65d
Ricardo Barbosa <ribeiro.kdu@gmail.com>
2022-10-02 20:26:40 -0300

    Add files via upload

---
+++ Trabalho/componentes/popaganda/script.js
@@ -0,0 +1,42 @@
+var div = document.getElementById('log');
+// pega o elemento
+
+var textos = ['Vendemos colas!     Oops...', 'Ofertamos\n SOLUÇÕES para alavancar seu desempenho acadêmico 🎓'];
+
+function escrever(str, done) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function() {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 500); // só para esperar um bocadinho
+        }
+        var next = char.pop();
+        div.innerHTML += next;
+    }, 75);
+}
+
+function limpar(done) {
+    var char = div.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(function() {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 10);
+}
+
+function rodape(conteudos, el) {
+    var atual = -1;
+	function prox(cb){
+		if (atual < conteudos.length - 1) atual++;
+		else atual = 0;
+		var str = conteudos[atual];
+		escrever(str, function(){
+			limpar(prox);
+		});
+	}
+	prox(prox);
+}
+rodape(textos);
