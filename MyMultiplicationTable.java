4e490ef9084b6750bdab138b41d4d92852b37045
Ricardo Barbosa <ribeiro.kdu@gmail.com>
2022-10-26 20:09:54 -0300

    MyMultiplicationTable

---
+++ MyMultiplicationTable.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=0;
+      int y=0;
+    for(int i = 0; i<=10; i++) {
+        int j=0;
+        for(j=0; j<=10; j++) {
+            System.out.println(i+"*"+j+"="+(i*j));
+        }
+        
+    }
+      System.out.println("Hello World!");
+    }
+}
